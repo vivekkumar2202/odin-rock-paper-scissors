bed108f7a8283367df058960ee88af55fc34ae58
Vivek <137711336+vivekkumar2202@users.noreply.github.com>
2024-12-16 13:30:12 +0000

    add getHumanChoice function

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function getComputerChoice() {
-    randomNumber = Math.random();
+    let randomNumber = Math.random();
     if (randomNumber < 1/3)
         return "rock";
     if (randomNumber > 2/3)
         return "paper";
     return "scissor";
+}
+
+function getHumanChoice() {
+    let choice = prompt("Choose rock, paper or scissor");
+    let choice_lower = choice.toLowerCase();
+    return choice_lower;
 }
